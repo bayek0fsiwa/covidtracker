b9762a5d9f94c526fc4e5a357e712c357bcafe90
bayek0fsiwa <imvikas@tutanota.com>
2021-04-25 01:44:41 +0530

    init

---
+++ src/components/Layout.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="layout">
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn href="https://www.covid19api.org/" target="_blank">
+      <v-icon>notranslate mdi mdi-virus theme--light</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      fixed: true,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Home",
+          to: "/",
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Graph",
+          to: "/graph",
+        },
+      ],
+      title: "Covid 19 smh!",
+    };
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h2>Hello</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
