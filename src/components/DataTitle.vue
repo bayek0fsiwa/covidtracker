74ef331af3f24c6dca2bbee3afbccff06f9fbaee
bayek0fsiwa <imvikas@tutanota.com>
2021-04-25 13:15:37 +0530

    datatitle

---
+++ src/components/DataTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="data-title">
+      <h2>{{text}}</h2>
+      <div>
+          {{dataDate}}
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'DataTitle',
+    props: ['text', 'dataDate'],
+}
+</script>
+
+<style>
+
+</style>
